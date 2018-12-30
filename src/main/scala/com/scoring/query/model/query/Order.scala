d0b4a4dbfa40f38905e870a00d421c90a5ce6ee4
Kalab Oster <kalaboster@wordsum.com>
2018-12-29 16:35:54 -0800

    Add query model

---
+++ src/main/scala/com/scoring/query/model/query/Order.scala
@@ -0,0 +1,12 @@
+package com.scoring.query.model.query
+
+class Order {
+
+  var stb: Int = 0
+  var title: Int = 0
+  var provider: Int = 0
+  var date: Int = 0
+  var rev: Int = 0
+  var viewTime: Int = 0
+
+}
