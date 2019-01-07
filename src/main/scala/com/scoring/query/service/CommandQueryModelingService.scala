9e2d811d46c49c698e88091823f3fc9767fb34c5
Kalab Oster <kalaboster@wordsum.com>
2019-01-06 21:31:10 -0800

    Add function to define the object of select for the query.

--- src/main/scala/com/scoring/query/service/CommandQueryModelingService.scala
+++ src/main/scala/com/scoring/query/service/CommandQueryModelingService.scala
@@ -1,14 +1,55 @@
 package com.scoring.query.service
 
-import com.scoring.query.model.query.ScoringQuery
+import com.scoring.query.model.query.{ScoringQuery, Select}
 import org.springframework.stereotype.Service
 
 @Service
 class CommandQueryModelingService extends CommandModelingService {
 
+  val regexCommand = "(-)(s|f|o)=.*".r
+  val regexSelect = "-s=.*"
+  var select = "-s="
+  var selectCSV = ","
+
   override def process(args: String*): ScoringQuery = {
     var scoringQuery: ScoringQuery = new ScoringQuery()
-    args.foreach(arg => println(arg))
+
+    for (a <- args.filter(s => regexCommand.findFirstIn(s).isDefined)) {
+      if (a.matches(regexSelect)) {
+        scoringQuery = select(scoringQuery, a)
+      }
+    }
+
+    return scoringQuery
+  }
+
+  def select(scoringQuery: ScoringQuery, arg: String): ScoringQuery = {
+
+    var selectModel: Select = new Select
+
+    for (s <- arg.stripPrefix(select).split(selectCSV)) {
+      if (s == "STB") {
+        selectModel.stb = true
+      }
+      if (s == "PROVIDER") {
+        selectModel.provider = true
+      }
+      if (s == "DATE") {
+        selectModel.date = true
+      }
+      if (s == "REV") {
+        selectModel.rev = true
+      }
+      if (s == "TITLE") {
+        selectModel.title = true
+      }
+      if (s == "VIEWTIME") {
+        selectModel.viewTime = true
+      }
+    }
+
+    scoringQuery.select = selectModel
+
     return scoringQuery
   }
 
