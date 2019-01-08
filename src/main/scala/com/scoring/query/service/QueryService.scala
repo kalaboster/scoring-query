913b8f836ec66a148f2d98dc0d21cd24c6eb0300
Kalab Oster <kalaboster@wordsum.com>
2019-01-07 20:39:39 -0800

    Write trait into commandline query service and a process signature.

--- src/main/scala/com/scoring/query/service/QueryService.scala
+++ src/main/scala/com/scoring/query/service/QueryService.scala
@@ -1,6 +1,10 @@
 package com.scoring.query.service
 
-class QueryService {
+import com.scoring.query.model.ScoringModel
+import com.scoring.query.model.query.ScoringQueryModel
 
+trait QueryService {
+
+  def process(query: ScoringQueryModel, url: String): List[ScoringModel]
 
 }
